f86edb277f6109ab89312072a6610b334c281074
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2021-07-19 16:36:11 -0400

    Improve missing page message

--- gen_docs.py
+++ gen_docs.py
@@ -234,7 +234,7 @@ def generate():
         bodyhtml = convert("".join(mdfile.readlines()),vers,lang)
         mdfile.close()
     else:
-        bodyhtml = "<p>(Documentation file not found)</p><p>If you are looking for documentation for the Julia version, in-depth documentation can be found <a href="https://itensor.github.io/ITensors.jl/stable/index.html">here</a></p>"
+        bodyhtml = "<p>Documentation file not found.</p><p>Documentation for the Julia version, can be found <b><a href=\"https://itensor.github.io/ITensors.jl/stable/index.html\">at this link.</a></b></p>"
 
     # Generate directory tree hyperlinks
     dirlist = page.split('/')
